0a117a429142e0c0fb9c37ca9c6badfaa5e78e8a
kiryun <rlgus1205@naver.com>
2020-04-14 23:48:40 +0900

    ReverseGeocoding

---
+++ mSNS_iOS/GeoCoding.swift
@@ -0,0 +1,48 @@
+//
+//  GeoCoding.swift
+//  mSNS_iOS
+//
+//  Created by Gihyun Kim on 2020/04/14.
+//  Copyright © 2020 wimes. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Geocoding{
+//    var lat: Double = 0.0
+//    var lon: Double = 0.0
+//    
+//    init(lat: Double, lon: Double) {
+//        self.lat = lat
+//        self.lon = lon
+//    }
+    
+    // https://stackoverflow.com/questions/46869394/reverse-geocoding-in-swift-4
+    func geocode(location: CLLocation, completion: @escaping (_ placemark: [CLPlacemark]?, _ error: Error?) -> Void)  {
+        CLGeocoder().reverseGeocodeLocation(location) { placemark, error in
+            guard let placemark = placemark, error == nil else {
+                completion(nil, error)
+                return
+            }
+            completion(placemark, nil)
+        }
+    }
+    
+    // https://greenchobo.tistory.com/8
+//    func convertToAddressWith(coordinate: CLLocation) {
+//        let geoCoder = CLGeocoder()
+//        geoCoder.reverseGeocodeLocation(coordinate){ (placemarks, error) -> Void in
+//            if error != nil{
+//                print("\(error)")
+//                return
+//            }
+//
+//            guard let placemarks = placemarks else {
+//                completion(nil, error)
+//                return
+//            }
+//            ccompletion(placemarks, nil)
+//        }
+//    }
+}
--- mSNS_iOS/TestView.swift
+++ mSNS_iOS/TestView.swift
@@ -7,16 +7,58 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
     let apiClient = ApiClient()
     
     var body: some View {
-        Button(action: self.req) {
-            Text("REQ")
+        List{
+            Button(action: self.req) {
+                Text("REQ")
+            }
+            Button(action: self.geocoding){
+                Text("GeoCoding")
+            }
         }
     }
 
+    func geocoding(){
+        // 경기도 수원시 장안구 수성로364번길 3
+        let location = CLLocation(latitude: 37.293418, longitude: 127.011916)
+        let geo: Geocoding = Geocoding()
+        geo.geocode(location: location) { placemark, error in
+            if let error = error as? CLError {
+                print("CLError:", error)
+                return
+            } else if let placemark = placemark?.first {
+                // you should always update your UI in the main thread
+                DispatchQueue.main.async {
+                    //  update UI here
+                    print("name:", placemark.name ?? "unknown")
+
+                    print("address1:", placemark.thoroughfare ?? "unknown")
+                    print("address2:", placemark.subThoroughfare ?? "unknown")
+                    print("neighborhood:", placemark.subLocality ?? "unknown")
+                    print("city:", placemark.locality ?? "unknown")
+
+                    print("state:", placemark.administrativeArea ?? "unknown")
+                    print("subAdministrativeArea:", placemark.subAdministrativeArea ?? "unknown")
+                    print("zip code:", placemark.postalCode ?? "unknown")
+                    print("country:", placemark.country ?? "unknown", terminator: "\n\n")
+
+                    print("isoCountryCode:", placemark.isoCountryCode ?? "unknown")
+                    print("region identifier:", placemark.region?.identifier ?? "unknown")
+
+                    print("timezone:", placemark.timeZone ?? "unknown", terminator:"\n\n")
+
+                    // Mailind Address
+//                    print(placemark.mailingAddress ?? "unknown")
+                }
+            }
+        }
+    }
+    
     func req(){
         let builder = ComponentBuilder()
         let director = ExternalComponentDirector(builder: builder)
