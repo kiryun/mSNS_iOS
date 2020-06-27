d75101881186ee25dc961e511c7c72d6c9f7c88a
kiryun <rlgus1205@naver.com>
2020-06-27 22:14:49 +0900

    [Rank/UI] Rank layout case#1

--- mSNS_iOS/UI/RankView.swift
+++ mSNS_iOS/UI/RankView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 struct RankView: View {
     var body: some View {
-        VStack{
-            GeometryReader{ g in
-                HStack{
-                    GeometryReader{ ge in
+        GeometryReader{ g in
+            VStack{
+                GeometryReader{ ge in
+                    HStack{
                         Rectangle()
                             .fill(Color.red)
                             .frame(width: ge.size.width/2, height: ge.size.height)
@@ -23,32 +23,59 @@ struct RankView: View {
                     }
                 }.frame(width: g.size.width, height: g.size.height/2)
                 
-                HStack{
-                    GeometryReader{ geo in
-                        VStack{
-                            GeometryReader{ geom in
+                GeometryReader{ geo in
+                    HStack{
+                        GeometryReader{ geom in
+                            VStack{
                                 Rectangle()
                                     .fill(Color.yellow)
-//                                    .frame(width: goem.size.width, height: goem.size.height/2)
+                                    .frame(width: geom.size.width, height: geom.size.height/2)
                                 Rectangle()
                                     .fill(Color.green)
-//                                    .frame(width: goem.size.width, height: goem.size.height/2)
+                                    .frame(width: geom.size.width, height: geom.size.height/2)
                             }
+                            
                         }.frame(width: geo.size.width/2, height: geo.size.height)
+                        
                         Rectangle()
                             .fill(Color.blue)
                             .frame(width: geo.size.width/2, height: geo.size.height)
                     }
                     
                 }.frame(width: g.size.width, height: g.size.height/2)
+                
             }
-            
         }
     }
 }
 
-struct RankView_Previews: PreviewProvider {
-    static var previews: some View {
-        RankView()
-    }
-}
+
+//HStack{
+//                    GeometryReader{ ge in
+//                        Rectangle()
+//                            .fill(Color.red)
+//                            .frame(width: ge.size.width/2, height: ge.size.height)
+//                        Rectangle()
+//                            .fill(Color.orange)
+//                            .frame(width: ge.size.width/2, height: ge.size.height)
+//                    }
+//                }.frame(width: g.size.width, height: g.size.height/2)
+//
+//                HStack{
+//                    GeometryReader{ geo in
+//                        VStack{
+//                            GeometryReader{ geom in
+//                                Rectangle()
+//                                    .fill(Color.yellow)
+////                                    .frame(width: goem.size.width, height: goem.size.height/2)
+//                                Rectangle()
+//                                    .fill(Color.green)
+////                                    .frame(width: goem.size.width, height: goem.size.height/2)
+//                            }
+//                        }.frame(width: geo.size.width/2, height: geo.size.height)
+//                        Rectangle()
+//                            .fill(Color.blue)
+//                            .frame(width: geo.size.width/2, height: geo.size.height)
+//                    }
+//
+//                }.frame(width: g.size.width, height: g.size.height/2)
