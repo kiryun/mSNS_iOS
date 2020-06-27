bbefa4ddb871fdd66d6d2882adbdbf2572b0ed6e
kiryun <rlgus1205@naver.com>
2020-06-27 22:17:00 +0900

    [Rank/UI] Rank layout case#1

--- mSNS_iOS/UI/RankView.swift
+++ mSNS_iOS/UI/RankView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 
 struct RankView: View {
     var body: some View {
+        RankLayoutCase1()
+    }
+}
+
+struct RankLayoutCase1: View {
+    var body: some View{
         GeometryReader{ g in
             VStack{
                 GeometryReader{ ge in
@@ -48,34 +54,3 @@ struct RankView: View {
         }
     }
 }
-
-
-//HStack{
-//                    GeometryReader{ ge in
-//                        Rectangle()
-//                            .fill(Color.red)
-//                            .frame(width: ge.size.width/2, height: ge.size.height)
-//                        Rectangle()
-//                            .fill(Color.orange)
-//                            .frame(width: ge.size.width/2, height: ge.size.height)
-//                    }
-//                }.frame(width: g.size.width, height: g.size.height/2)
-//
-//                HStack{
-//                    GeometryReader{ geo in
-//                        VStack{
-//                            GeometryReader{ geom in
-//                                Rectangle()
-//                                    .fill(Color.yellow)
-////                                    .frame(width: goem.size.width, height: goem.size.height/2)
-//                                Rectangle()
-//                                    .fill(Color.green)
-////                                    .frame(width: goem.size.width, height: goem.size.height/2)
-//                            }
-//                        }.frame(width: geo.size.width/2, height: geo.size.height)
-//                        Rectangle()
-//                            .fill(Color.blue)
-//                            .frame(width: geo.size.width/2, height: geo.size.height)
-//                    }
-//
-//                }.frame(width: g.size.width, height: g.size.height/2)
